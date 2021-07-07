d462c1cec7f66f0ef70edc8f4975e996394ebc6b
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-07-07 21:38:59 +0200

    color-switch with set interval

---
+++ src/js/color-switch.js
@@ -0,0 +1,36 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const startButton = document.querySelector('button[data-action="start"]');
+const stopButton = document.querySelector('button[data-action="stop"]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
+
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+startButton.addEventListener('click', onStartClick);
+stopButton.addEventListener('click', onStopClick);
+
+
+function onStartClick() {
+    timerId = setInterval(() => {
+        const randomColor = randomIntegerFromInterval(0, colors.length - 1);
+        bodyEl.style.backgroundColor = colors[randomColor];
+        startButton.removeEventListener('click', onStartClick);
+    }, 1000)
+}
+
+function onStopClick() {
+    clearInterval(timerId);
+    startButton.addEventListener('click', onStartClick);
+}
+
